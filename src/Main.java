a7105408182db0c583cb58b62b88da9dc47bc17a
Solpostgit <begemot_off@mail.ru>
2023-08-28 23:22:32 +0300

    Changed all tasks

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,30 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
-        String firstName = "Ivan ";
+        String firstName = "Ivan";
         String middleName = "Ivanovich";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.printf("ФИО сотрудника - %s\n", fullName);
     }
 
     public static void task2() {
         System.out.println();
         System.out.println("Task 2");
-        String firstName = "Ivan ";
+        String firstName = "Ivan";
         String middleName = "Ivanovich";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + fullName.toUpperCase());
     }
 
     public static void task3() {
         System.out.println();
         System.out.println("Task 3");
-        String firstName = "Семён ";
+        String firstName = "Семён";
         String middleName = "Семёнович";
-        String lastName = "Иванов ";
-        String fullName = lastName + firstName + middleName;
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
         fullName = fullName.replace('ё', 'е');
         System.out.printf("Данные ФИО сотрудника  — %s\n", fullName);
     }
