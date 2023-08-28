422fd2966144433010dbe1beb2b16d73bb4fa5a0
Solpostgit <begemot_off@mail.ru>
2023-08-28 22:47:56 +0300

    All tasks completed

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();/*
-        task1();
-        task1();*/
+        task3();
     }
 
     public static void task1() {
