5d3d160aaa52006f1c1519bc80598312eb6f403b
Solpostgit <begemot_off@mail.ru>
2023-08-28 22:34:20 +0300

    Added task2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        task1();/*
         task1();
+        task2();/*
         task1();
         task1();
         task1();*/
@@ -13,6 +13,16 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
-        System.out.printf("ФИО сотрудника - %s", fullName);
+        System.out.printf("ФИО сотрудника - %s\n", fullName);
+    }
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Task 2");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + fullName.toUpperCase());
     }
 }
